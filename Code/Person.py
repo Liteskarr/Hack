3668bab467b70b644445ca36201887d6ea2e99fe
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-04-18 16:14:22 +0300

    1

--- Code/Person.py
+++ Code/Person.py
@@ -8,19 +8,19 @@ class Person:
 
 class Father(Person):
     def __init__(self):
-        self.name =  ''
-        self.surname =  ''
-        self.age =  random.randint(20, 60)
+        self.name = ''
+        self.surname = ''
+        self.age = random.randint(20, 60)
         self.condition = []
         super().__init__()
 
 
 class Mom(Person):
     def __init__(self):
-        self.name =  ''
-        self.surname =  ''
-        self.age =  0
-        self.age =  random.randint(20, 55)
+        self.name = ''
+        self.surname = ''
+        self.age = 0
+        self.age = random.randint(20, 55)
         self.condition = []
         super().__init__()
 
