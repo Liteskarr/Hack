a515e98e45dd86d2e45567784a25b451964e6a4b
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-04-18 16:02:57 +0300

    1

--- venv/qwer.py
+++ venv/qwer.py
@@ -20,7 +20,7 @@ class Mom(Person):
         self.name =  ''
         self.surname =  ''
         self.age =  0
-        self.age =  random.randint( 20, 55)
+        self.age =  random.randint(20, 55)
         self.condition = []
         super().__init__()
 
