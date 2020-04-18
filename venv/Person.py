88457cfd9bf3ca0ce2ed13f1f7d2b3a8c06345b7
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-04-18 14:51:21 +0300

    персонажи

--- venv/Person.py
+++ venv/Person.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Person:
     def __init__(self):
         pass
@@ -7,7 +10,7 @@ class Father(Person):
     def __init__(self):
         self.name = ''
         self.surname = ''
-        self.age = 0
+        self.age = random.randint(20, 60)
         self.condition = []
         super().__init__()
 
@@ -16,7 +19,7 @@ class Mom(Person):
     def __init__(self):
         self.name = ''
         self.surname = ''
-        self.age = 0
+        self.age = random.randint(20, 55)
         self.condition = []
         super().__init__()
 
