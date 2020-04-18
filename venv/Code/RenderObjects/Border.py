078185b2d1847acb4a87d8998ff38d40dfec5348
Liteskarr <398501z3@gmail.com>
2020-04-18 19:16:11 +0300

    CoreCommit

--- venv/Code/RenderObjects/Border.py
+++ venv/Code/RenderObjects/Border.py
@@ -2,9 +2,13 @@ import Code.IRenderObject as IRenderObject
 
 
 class Border(IRenderObject.IRenderObject):
-    def __int__(self):
-        pass
+    def __init__(self, char):
+        self.char = char
 
     @override
     def darw(self, target: Canvas):
-        pass
+        target.set_symphol(self.char)
+        target.Line(0, 0, target.sizeX, 0)
+        target.Line(0, 0, 0, target.sizeY)
+        target.Line(target.sizeX, 0, target.sizeX, target.sizeY)
+        target.Line(0, target.sizeY, target.sizeX, target.sizeY)
