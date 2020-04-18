b93a0864dfca34fb201db18a061308efbb3aa334
Liteskarr <398501z3@gmail.com>
2020-04-18 19:16:50 +0300

    CoreCommit

--- venv/Code/RenderObjects/Border.py
+++ venv/Code/RenderObjects/Border.py
@@ -8,7 +8,7 @@ class Border(IRenderObject.IRenderObject):
     @override
     def darw(self, target: Canvas):
         target.set_symphol(self.char)
-        target.Line(0, 0, target.sizeX, 0)
-        target.Line(0, 0, 0, target.sizeY)
-        target.Line(target.sizeX, 0, target.sizeX, target.sizeY)
-        target.Line(0, target.sizeY, target.sizeX, target.sizeY)
+        target.Line(0, 0, target.sizeX - 1, 0)
+        target.Line(0, 0, 0, target.sizeY - 1)
+        target.Line(target.sizeX - 1, 0, target.sizeX - 1, target.sizeY - 1)
+        target.Line(0, target.sizeY - 1, target.sizeX - 1, target.sizeY - 1)
