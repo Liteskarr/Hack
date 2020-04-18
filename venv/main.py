0b46ac83e5acba44c6ed1d5d8a2a8c3d62cc9e0c
MR-Geri <63905039+MR-Geri@users.noreply.github.com>
2020-04-18 14:07:38 +0300

    персонажи

--- venv/main.py
+++ venv/main.py
@@ -1,2 +1,31 @@
-print('123456')
-print('111123')
+class Players:
+    def __init__(self):
+        pass
+
+    class Father:
+        def __init__(self):
+            self.name = ''
+            self.surname = ''
+            self.age = 0
+            self.condition = []
+
+    class Mom:
+        def __init__(self):
+            self.name = ''
+            self.surname = ''
+            self.age = 0
+            self.condition = []
+
+    class Con:
+        def __init__(self):
+            self.name = ''
+            self.surname = ''
+            self.age = 0
+            self.condition = []
+
+    class Daughter:
+        def __init__(self):
+            self.name = ''
+            self.surname = ''
+            self.age = 0
+            self.condition = []
