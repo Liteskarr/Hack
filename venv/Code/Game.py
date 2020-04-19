b04ce61f47a91d8baed31a11e740ede64877b111
Liteskarr <398501z3@gmail.com>
2020-04-19 13:56:24 +0300

    CoreCommit

--- venv/Code/Game.py
+++ venv/Code/Game.py
@@ -1,24 +1,28 @@
 import threading
 import os
-import msvcrt
+
+import keyboard
 
 from Code.Canvas import Canvas
 
 
 class Game:
-    def __init__(self, canvas=Canvas(100, 50)):
+    def __init__(self, canvas=Canvas(100, 45)):
         self.is_run = True
         self.loaded_scene = None
         self.canvas = canvas
 
     def load_scene(self, scene):
         self.loaded_scene = scene
+        keyboard.hook(scene.listen_clicks)
+        scene.start()
 
     def draw(self):
-        os.system(f'mode con cols={self.canvas.sizeX} lines={self.canvas.sizeY}')
+        os.system(f'mode con cols={self.canvas.sizeX} lines={self.canvas.sizeY + 1}')
         os.system('cls')
-        for rend in self.loaded_scene:
-            self.canvas(rend)
+        for rend in self.loaded_scene.renders:
+            self.canvas.add_object(rend)
+        self.canvas.draw()
 
     def start(self):
         if self.loaded_scene is None:
@@ -27,4 +31,4 @@ class Game:
 
         while self.is_run:
             self.loaded_scene.update()
-            draw()
+            self.draw()
