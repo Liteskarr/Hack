4ef19f7771de4430b7155c5809f8782e325befdc
Liteskarr <398501z3@gmail.com>
2020-04-19 11:49:03 +0300

    CoreCommit

--- venv/Code/Program.py
+++ venv/Code/Program.py
@@ -1,21 +1,19 @@
 import os
-import Code.Canvas as Canvas
-import Code.RenderObjects.Border as Border
-import Code.RenderObjects.Button as Button
+#
 import colorama
 from colorama import Back
+#
+from Code.Game import Game
+from Code.Scenes.MainMenu import main_menu
+#
 colorama.init()
-
 
 def default_update():
     os.system("mode con cols=100 lines=49")
 
 default_update()
 
-can = Canvas.Canvas(10, 10)
-
-b = Button.Button(1, 1)
-
-can.draw()
-
-input()
+game = Game()
+game.load_scene(main_menu)
+game.start()
+game.join()
